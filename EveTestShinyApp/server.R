365db0bca0637509f8bab7d9648018e2c0ba9b77
Eve Washington <eve.suane@gmail.com>
2021-02-16 01:16:14 -0500

    so close

--- EveTestShinyApp/server.R
+++ EveTestShinyApp/server.R
@@ -39,115 +39,44 @@ park_poly <- read.socrata("https://data.cityofnewyork.us/dataset/Social-Distanci
 park_poly <- sf::st_as_sf( park_poly, wkt = "multipolygon")
 
 violations <-  read.socrata("https://data.cityofnewyork.us/dataset/Social-Distancing-Parks-Crowds-Data/gyrw-gvqc") %>%
-  group_by(park_area_id) %>% 
+  group_by(park_area_id, encounter_timestamp) %>% 
   summarise(patrons = sum(patroncount))
 
 parks_with_p <- merge(park_poly,violations,by="park_area_id", all.x=TRUE)
 
+bins <- c(0, 10, 25, 50, 100, 200, 400, 800, Inf)
+pal <- colorBin("YlOrRd", domain = parks_with_p$patrons, bins = bins)
+
+
 #shiny vibes
 function(input, output, session) {
-  
-  ## Interactive Map ###########################################
+  #make it move
+  filteredData <- reactive({ parks_with_p[
+    which(
+      parks_with_p$park_borough %in% input$Borough &
+      parks_with_p$encounter_timestamp >= input$range[1] &
+      parks_with_p$encounter_timestamp <= input$range[2]
+      )
+    ]
+  })
   
   # Create the map
-  output$map <- renderLeaflet({
-    leaflet(data = parks_with_p) %>%
-      addTiles() %>%
-      addPolygons(label = ~park_area_desc)
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      addPolygons(data = filteredData()$geometry,
+                  label = ~filteredData()$park_area_desc, 
+                  fillColor = ~pal(filteredData()$patrons),
+                  weight = .5,
+                  opacity = 5,
+                  color = "white",
+                  dashArray = "3",
+                  fillOpacity = 0.7) #%>%
+      # addLegend("bottomright", pal = pal, values = ~patrons,
+      #           title = "Number of Patrons Violating Social Distancing ",
+      #           opacity = 1
+      #)
   })
   
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  parksInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(zipdata[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-    
-    subset(parks_with_p,
-           latitude >= latRng[1] & latitude <= latRng[2] &
-             longitude >= lngRng[1] & longitude <= lngRng[2])
-  })
-  
-  output$histCentile <- renderPlot({
-    # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
-      return(NULL)
-    
-    hist(zipsInBounds()$centile,
-         breaks = centileBreaks,
-         main = "SuperZIP score (visible zips)",
-         xlab = "Percentile",
-         xlim = range(allzips$centile),
-         col = '#00DD00',
-         border = 'white')
-  })
-  
-  output$scatterCollegeIncome <- renderPlot({
-    # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
-      return(NULL)
-    
-    print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  })
-  
-  # This observer is responsible for maintaining the circles and legend,
-  # according to the variables the user has chosen to map to color and size.
-  observe({
-    colorBy <- input$color
-    sizeBy <- input$size
-    
-    if (colorBy == "superzip") {
-      # Color and palette are treated specially in the "superzip" case, because
-      # the values are categorical instead of continuous.
-      colorData <- ifelse(zipdata$centile >= (100 - input$threshold), "yes", "no")
-      pal <- colorFactor("viridis", colorData)
-    } else {
-      colorData <- zipdata[[colorBy]]
-      pal <- colorBin("viridis", colorData, 7, pretty = FALSE)
-    }
-    
-    if (sizeBy == "superzip") {
-      # Radius is treated specially in the "superzip" case.
-      radius <- ifelse(zipdata$centile >= (100 - input$threshold), 30000, 3000)
-    } else {
-      radius <- zipdata[[sizeBy]] / max(zipdata[[sizeBy]]) * 30000
-    }
-    
-    leafletProxy("map", data = zipdata) %>%
-      clearShapes() %>%
-      addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-                 stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-      addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-                layerId="colorLegend")
-  })
-  
-  # Show a popup at the given location
-  showZipcodePopup <- function(zipcode, lat, lng) {
-    selectedZip <- allzips[allzips$zipcode == zipcode,]
-    content <- as.character(tagList(
-      tags$h4("Score:", as.integer(selectedZip$centile)),
-      tags$strong(HTML(sprintf("%s, %s %s",
-                               selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
-      ))), tags$br(),
-      sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
-      sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
-      sprintf("Adult population: %s", selectedZip$adultpop)
-    ))
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
-  }
-  
-  # When map is clicked, show a popup with city info
-  observe({
-    leafletProxy("map") %>% clearPopups()
-    event <- input$map_shape_click
-    if (is.null(event))
-      return()
-    
-    isolate({
-      showZipcodePopup(event$id, event$lat, event$lng)
-    })
-  })
   
 }
--- EveTestShinyApp/ui.R
+++ EveTestShinyApp/ui.R
@@ -18,26 +18,18 @@ vars <- c(
 )
 
 
-navbarPage("Parks", id="nav",
-           
-           tabPanel("Interactive map",
-                    div(class="outer",
-                        
-                        # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                        leafletOutput("map", width="100%", height="100%"),
-                        
-                        # Shiny versions prior to 0.11 should use class = "modal" instead.
-                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                      width = 330, height = "auto",
-                                      
-                                      h2("Park explorer"),
-                                      
-                                      selectInput("color", "Color", vars),
-                                      
-                                      plotOutput("violations", height = 200),
-                      
-                        )
-                    )
-           )
+fluidPage(
+  leafletOutput("mymap"),
+  absolutePanel(top = 10, right = 10,
+                sliderInput("range", "Date of Social Distancing Violation", 
+                            min(violations$encounter_timestamp), max(violations$encounter_timestamp),
+                            value = range(violations$encounter_timestamp)
+                ),
+                span(tags$i(h4("Select Parks by Borough"))),
+                checkboxGroupInput("Borough", "Which boroughs are you interested in?",
+                                   choices = c("Manhattan", "Brooklyn", "Bronx",
+                                               "Queens", "Staten Island"),
+                                   selected = c("Manhattan", "Brooklyn", "Bronx",
+                                                "Queens", "Staten Island"))
+   )
 )
