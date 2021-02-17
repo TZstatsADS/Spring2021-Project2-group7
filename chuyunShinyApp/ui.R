9325576f6f32b87d49473f908f3522b6e3076f6c
Eve Washington <eve.suane@gmail.com>
2021-02-16 23:21:31 -0500

    fix openstreets

--- chuyunShinyApp/ui.R
+++ chuyunShinyApp/ui.R
@@ -15,14 +15,12 @@ library(haven)
 library(devtools)
 library(RColorBrewer)
 library(RSocrata)
+library(shinyTime)
 
 open_street <- read.socrata("https://data.cityofnewyork.us/Health/Open-Streets-Locations/uiay-nctu")
 
 
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Map of NYC Open Streets"),
   
   mainPanel(leafletOutput("map")),
   #Create master panel with different widgets for specification
@@ -35,19 +33,21 @@ ui <- fluidPage(
                 
                 
                 #Widget that filters open streets by borough (can select multiple)
-                span(tags$i(h4("Select Open Streets by Borough"))),
-                checkboxGroupInput("Borough", "Which boroughs are you interested in?",
+                span(tags$i(h3("Find an Openstreet"))),
+                span(tags$i(h4("Filter Open Streets by Borough"))),
+                helpText("Tip! You much have at least one borough selected"),
+                checkboxGroupInput("boroughst", "Which boroughs are you interested in?",
                                    choices = c("Manhattan", "Brooklyn", "Bronx",
                                                "Queens", "Staten Island"),
                                    selected = c("Manhattan", "Brooklyn", "Bronx",
                                                 "Queens", "Staten Island")),
                 
                 #Widget that filters openstreets by day of week
-                span(tags$i(h4("Select Open Streets by Day of Week"))),
-                checkboxGroupInput("Day of Week", "What days of the week is this open street becoming public",
-                                   choices = c("M,T,W,R,F","TWRFSU", "F,S","S,U","FSU","RFSU","ALL"),
-                                   selected = c("M,T,W,R,F","TWRFSU", "F,S","S,U","FSU","RFSU","ALL")
+                span(tags$i(h4("Filter Open Streets by Day of Week"))),
+                selectInput("Day", "What day of the week would you like to vist a Street?",
+                                   choices = c("All", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
                 )
+                
   )
 )
 
