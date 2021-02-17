d96a93cb58e884fa4dbf95bdab905209da8e30a0
Eve Washington <eve.suane@gmail.com>
2021-02-17 01:30:22 -0500

    added open streets

--- FinalApp/server.R
+++ FinalApp/server.R
@@ -51,6 +51,11 @@ all_parks_with_p <- merge(park_poly,all_violations,by="park_area_id", all.x=TRUE
 
 all_parks_with_p$encounter_datetime <- anytime::anydate(all_parks_with_p$encounter_datetime)
 
+#OpenStreet Data
+open_street <- read.socrata("https://data.cityofnewyork.us/Health/Open-Streets-Locations/uiay-nctu")
+
+open_street <- sf::st_as_sf(open_street, wkt = "the_geom")
+
 
 
 # Define server logic required to draw a histogram
@@ -127,6 +132,37 @@ shinyServer(function(input, output) {
     })
   
     output$restaurant_table <- renderDataTable(ny_restaurant_table)
+    
+    #Open Streets --------------------------------------------------------------------------------------------
+    #Allow dataset to be manipulated by the shiny app ui
+    shiny_open_street <- reactive(open_street %>%
+                                    filter(borough %in% input$boroughst) %>%
+                                    {if(input$Day == "Monday") filter(., strptime(monday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                        strptime(monday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>%
+                                    {if(input$Day == "Tuesday") filter(., strptime(tuesday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                         strptime(tueday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>%
+                                    {if(input$Day == "Wednesday") filter(., strptime(wednesday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                           strptime(wednesday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>%
+                                    {if(input$Day == "Thursday") filter(., strptime(thursday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                          strptime(thursday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>%
+                                    {if(input$Day == "Friday") filter(., strptime(friday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                        strptime(friday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>%  
+                                    {if(input$Day == "Saturday") filter(., strptime(saturday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                          strptime(saturday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . } %>% 
+                                    {if(input$Day == "Sunday") filter(., strptime(sunday_start, "%I:%M%p") <= strptime(input$Time, "%I:%M%p") & 
+                                                                        strptime(sunday_end, "%I:%M%p") > strptime(input$Time, "%I:%M%p")) else . }%>%
+                                    {if(nrow(.)==0)  showNotification("no streets exist that meet such criteria, try picking another time", type = "error", duration = 15) else . }
+    )
+    
+    
+    
+    #output the map in the server
+    output$map <-  renderLeaflet({
+      leaflet(data = shiny_open_street()) %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        addPolygons(label = ~on_street)
+      
+    })
     
     
     #parks --------------------------------------------------------------------------------------------------------------
--- FinalApp/ui.R
+++ FinalApp/ui.R
@@ -121,8 +121,79 @@ shinyUI(
                                                          mainPanel(
                                                            column(1, dataTableOutput("restaurant_table"))))
                          )),
-               tabPanel("Open Stree
-               tabPanel("Open Streets"),
+               
+               tabPanel("Open Streets", 
+                        fillPage( tags$head( tags$style( HTML(".shiny-notification {
+                                position:fixed;
+                                top: 10vh;
+                                left: 10vw;
+                                  }"))),
+                                  
+                                  tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
+                                  leafletOutput("map"),
+                                  #Create master panel with different widgets for specification
+                                  absolutePanel(fixed = TRUE, class = "panel panel-default", draggable = TRUE, top = 90, left = "auto", 
+                                                right = 20, bottom = "auto", width = 330, height = "auto",
+                                                
+                                                
+                                                
+                                                #Widget that filters open streets by borough (can select multiple)
+                                                span(tags$i(h3("Find an Openstreet"))),
+                                                span(tags$i(h4("Filter Open Streets by Borough"))),
+                                                helpText("Tip! You much have at least one borough selected"),
+                                                checkboxGroupInput("boroughst", "Which boroughs are you interested in?",
+                                                                   choices = c("Manhattan", "Brooklyn", "Bronx",
+                                                                               "Queens", "Staten Island"),
+                                                                   selected = c("Manhattan", "Brooklyn", "Bronx",
+                                                                                "Queens", "Staten Island")),
+                                                
+                                                #Widget that filters openstreets by day of week
+                                                
+                                                selectInput("datetime", "Do you want to fliter by date and time?",
+                                                            choices = c("No", "Yes")),
+                                                
+                                                conditionalPanel("input.datetime == 'Yes'", 
+                                                                 selectInput("Day", "What day of the week would you like to vist a Street?",
+                                                                             choices = c("Select", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+                                                                 ),
+                                                                 
+                                                                 selectInput("Time", "What time would you like to vist a Street?",
+                                                                             choices = c("12:00AM",
+                                                                                         "1:00AM",
+                                                                                         "2:00AM",
+                                                                                         "3:00AM",
+                                                                                         "4:00AM",
+                                                                                         "5:00AM",
+                                                                                         "6:00AM",
+                                                                                         "7:00AM",
+                                                                                         "8:00AM",
+                                                                                         "9:00AM",
+                                                                                         "10:00AM",
+                                                                                         "11:00AM",
+                                                                                         "12:00PM",
+                                                                                         "1:00PM",
+                                                                                         "2:00PM",
+                                                                                         "3:00PM",
+                                                                                         "4:00PM",
+                                                                                         "5:00PM",
+                                                                                         "6:00PM",
+                                                                                         "7:00PM",
+                                                                                         "8:00PM",
+                                                                                         "9:00PM",
+                                                                                         "10:00PM",
+                                                                                         "11:00PM")
+                                                                 )
+                                                                 
+                                                                 
+                                                )
+                                                
+                                                
+                                                
+                                  ))
+                        
+                        
+                        
+                        ),
                tabPanel("Parks", 
                         fillPage(
                             tags$style(type = "text/css", "#parkmap {height: calc(100vh - 100px) !important;}"),
