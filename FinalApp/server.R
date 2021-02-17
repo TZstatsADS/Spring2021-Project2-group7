f32c9aeab51b1971bac5a8748a3e3024a31d3ec4
Peter Kwauk <60371559+GigaPK@users.noreply.github.com>
2021-02-17 11:49:59 -0500

    Update server.R
    Fixed error in restaurants

--- FinalApp/server.R
+++ FinalApp/server.R
@@ -113,7 +113,7 @@ shinyServer(function(input, output) {
             addCircles(lng = shiny_restaurants()$longitude,
                        lat = shiny_restaurants()$latitude, 
                        label = sprintf(
-                   "<strong>%s</strong><br/>%g recent positive cases in zip code<br/>%s<br/>%g sq.ft. of dining<br/>%s %g",
+                   "<strong>%s</strong><br/>%s recent positive cases in zip code<br/>%s<br/>%g sq.ft. of dining<br/>%s %g",
                    shiny_restaurants()$restaurant_name,  shiny_restaurants()$people_positive, shiny_restaurants()$seating_interest_sidewalk,
                    shiny_restaurants()$area, shiny_restaurants()$business_address,  shiny_restaurants()$zip) %>% 
                    lapply(htmltools::HTML),
